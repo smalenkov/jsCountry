4f5719d72aed5c4ee1b71c041b1856e638246014
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-04-14 10:46:04 +0300

    Hello

--- js/scripts/mainjs.js
+++ js/scripts/mainjs.js
@@ -1,56 +1,56 @@
 $(function () {
-    var str = "СПЕЦПРЕДЛОЖЕНИЯ ДЕЙСТВУЮЩИЕ ТОЛЬКО НА СТЕНДЕ:";
+  var str = "СПЕЦПРЕДЛОЖЕНИЯ ДЕЙСТВУЮЩИЕ ТОЛЬКО НА СТЕНДЕ:";
 
-    function enternum() {
-        var val = prompt("Введи число от 1 до 4");
-        swi(+val);
-    }
+  function enternum() {
+    var val = prompt("Введи число от 1 до 4");
+    swi(+val);
+  }
 
-    var Man = function (name, years, sex) {
-        this.name = name;
-        this.years = years;
-        this.sex = sex;
-    }
+  var Man = function (name, years, sex) {
+    this.name = name;
+    this.years = years;
+    this.sex = sex;
+  }
 
-    var Man1 = new Man("Николай", 13, "men");
+  var Man1 = new Man("Николай", 13, "men");
 
 //    enternum();
 
-    function swi(e) {
-        switch (e) {
-            case 1:
-                alert("One");
-                enternum();
-                break;
-            case 2:
-                alert("Two");
-                enternum();
-                break;
-            case 3:
-                alert("Three");
-                enternum();
-                break;
-            case 4:
-                alert("Four");
-                enternum();
-                break;
-        }
+  function swi(e) {
+    switch (e) {
+      case 1:
+        alert("One");
+        enternum();
+        break;
+      case 2:
+        alert("Two");
+        enternum();
+        break;
+      case 3:
+        alert("Three");
+        enternum();
+        break;
+      case 4:
+        alert("Four");
+        enternum();
+        break;
     }
+  }
 
 //  $("strong", "#mainp").html(Oleg.name);
-    function printif() {
-        if (Man1.name) {
-            $("#mainp > strong").html(Man1.name);
-            $("#mainp > strong").css({"font-size": "24px", "margin-bottom": "10px", "display": "block"});
-        } else {
-            $("#mainp > strong").html(str.toLowerCase());
-        }
+  function printif() {
+    if (Man1.name) {
+      $("#mainp > strong").html(Man1.name);
+      $("#mainp > strong").css({"font-size": "24px", "margin-bottom": "10px", "display": "block"});
+    } else {
+      $("#mainp > strong").html(str.toLowerCase());
     }
+  }
 
-    $("button").bind("click", function() {
-        printif();
-        $(this).remove();
-        $("#news").load("ajax.php");
-    });
+  $("button").bind("click", function () {
+    printif();
+    $(this).remove();
+    $("#news").load("ajax.php");
+  });
 
 });
