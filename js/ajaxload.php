42e1e05d7992006b0b6b2561c39a8e93ee4254b2
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-04-25 17:19:16 +0300

    Ajax php compact

--- js/ajaxload.php
+++ js/ajaxload.php
@@ -12,8 +12,11 @@ for ($i=0; $i<mysql_num_rows($result); $i++)	{
 
     $data = mysql_fetch_array($result);
 
-    echo "<a href='#/$data[areal_nameen]'>$data[areal_name]</a><br/>";
+//    echo "<a href='#/$data[areal_nameen]'>$data[areal_name]</a><br/>";
+    $arrstr[$i] = array($data[areal_nameen], $data[areal_name], $data[areal_text]);
 }
+
+echo json_encode($arrstr, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
 
 mysql_close($dbconnect);
 
